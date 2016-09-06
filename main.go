e54423210c239d43118b118b7a051f3167e7916c
Brent Martin <bmart35@gmail.com>
2016-09-05 23:24:33 -0500

    added a router for todo index

--- main.go
+++ main.go
@@ -11,9 +11,14 @@ import (
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", Index)
+	router.HandleFunc("/todos", TodoIndex)
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
 func Index(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Welcome!")
+}
+
+func TodoIndex(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Todo Index!")
 }
