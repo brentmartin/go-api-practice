e8858f929616c9cda4e0666f1139f65fbbc80539
Brent Martin <bmart35@gmail.com>
2016-09-05 23:13:50 -0500

    set it up to serve locally to http://localhost:8080

--- main.go
+++ main.go
@@ -11,4 +11,5 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 	})
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
